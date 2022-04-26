798e1af9941a4b9e726810b19684475b5891769b
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-26 14:07:16 -0500

    Agregando el objeto explores en node

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ app.get("/", (req, res) => {
 app.get("/launchx", (req, res) => {
   res.send("Bienvenidos a Launch X");
 });
+//Regresando un objeto
+//localhost:3000/explorersInNode
+app.get("/explorersInNode", (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello" };
+  res.send(explorer);
+});
